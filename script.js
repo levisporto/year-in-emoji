185335da472f6d9e7dc70f35919a061a23992808
Levi S. Porto <62207875+levisporto@users.noreply.github.com>
2025-05-06 17:01:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+function poopfest(){
+    const emoji1 = document.getElementById("emoji1");
+    const emoji2 = document.getElementById("emoji2");
+    const emoji3 = document.getElementById("emoji3");
+    const emoji4 = document.getElementById("emoji4");
+    const emoji5 = document.getElementById("emoji5");
+    emoji1.src = "https://emojiapi.dev/api/v1/pile_of_poo/120.png"
+     emoji2.src = "https://emojiapi.dev/api/v1/pile_of_poo/120.png"
+      emoji3.src = "https://emojiapi.dev/api/v1/pile_of_poo/120.png"
+       emoji4.src = "https://emojiapi.dev/api/v1/pile_of_poo/120.png"
+        emoji5.src = "https://emojiapi.dev/api/v1/pile_of_poo/120.png"
+    
+    
+    const h1 = document.getElementById("h1");
+        h1.textContent="Oh no!";
+        
+            const h2 = document.getElementById("h2");
+        h2.textContent="Hey! Don't be boring.";
+
+        const firstButton = document.getElementById("firstButton");
+        firstButton.textContent="Ok, fine";
+
+        const secondButton = document.getElementById("secondButton");
+        secondButton.textContent="Damn";
+
+}
+
+
+function randomEmojis(){
+    const emoji1 = document.getElementById("emoji1");
+    const emoji2 = document.getElementById("emoji2");
+    const emoji3 = document.getElementById("emoji3");
+    const emoji4 = document.getElementById("emoji4");
+    const emoji5 = document.getElementById("emoji5");
+    h1.textContent="Oh no!";
+  
+}
